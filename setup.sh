e768509956abf4559989ddfc425f41518c1862f9
Tom Wallis <github@tomwallis.net>
2016-01-14 18:04:46 +0000

    Added vim properly

--- setup.sh
+++ setup.sh
@@ -7,3 +7,9 @@ mv ~/.vim ~/.vim.orig
 # Create symbolic links to the rcfiles versions of our rc files
 ln -s -f `pwd -P`/zshrc ~/.zshrc
 ln -s -f `pwd -P`/vim ~/.vim
+
+# Set up vim properly
+cd vim
+./setupPlugins.sh
+./updatePlugins.sh
+cd ..
