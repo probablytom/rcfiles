d3e8135604fc2389ea174cfe8061098e1815f281
Tom Wallis <github@tomwallis.net>
2016-01-14 16:48:09 +0000

    Added vim

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 # Setup script for my .rc files
 
-rm ~/.zshrc
-ln -s -f zshrc ~/.zshrc
+# Move files that we'll link to symbolically rather than delete so we save old versions
+mv ~/.zshrc ~/.zshrc.orig
+mv ~/.vim ~/.vim.orig
+
+# Create symbolic links to the rcfiles versions of our rc files
+ln -s -f `pwd -P`/zshrc ~/.zshrc
+ln -s -f `pwd -P`/vim ~/.vim
