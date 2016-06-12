a487dc485b4fe04377722bf6f4aeacda5f2660a6
Tom Wallis <github@tomwallis.net>
2016-06-12 23:27:08 +0100

    Added better zsh extendability linking to ~/.oh-my-zsh

--- update.sh
+++ update.sh
@@ -7,4 +7,7 @@ git pull
 ./updateplugins.sh
 cd ..
 
+# Resync any new changes to custom zsh setup in ~/.oh-my-zsh/custom
+rsync -a ./zsh/custom ~/.oh-my-zsh
+
 # Done. Updates should propagate on next zsh session &c as a result of symlinks from setup.sh
--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,2 +1,5 @@
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Sync customisations
+rsync -a ~/Documents/rcfiles/zsh/custom ~/.oh-my-zsh
