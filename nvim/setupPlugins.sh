93d290d343f2fb106e59a3c0505017f99141f91d
Tom Wallis <github@tomwallis.net>
2016-06-27 23:29:18 +0100

    Completing the move to dein; will test on a new system

--- nvim/setupPlugins.sh
+++ nvim/setupPlugins.sh
@@ -1,4 +1,4 @@
-# Made by Tom Wallis, 2014-2015. 
+# Made by Tom Wallis, 2014-2016. 
 #To be run on a new system, to get packages &c set up and get vim just the way you like it.
 
 # Install and set up Pathogen.
@@ -28,3 +28,12 @@ cd ~/.vim && git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
 cd ~/.vim/bundle && git clone git@github.com:godlygeek/tabular.git    # Tabular
 git clone git@github.com:plasticboy/vim-markdown.git                 # Plasticboy's vim-markdown
 
+# We'll also neet to install some pip packages to get Python stuff working.
+pip install -r requirements.txt
+pip3 install -r requirements.txt
+
+# For some things we're moving to dein, too:
+mkdir -p ~/.rcfiles/nvim/dein
+git clone https://github.com/Shougo/dein.vim.git \
+	~/.rcfiles/nvim/dein
+source ~/.rcfiles/nvim/dein/bin/installer.sh  # Go through the installation process
